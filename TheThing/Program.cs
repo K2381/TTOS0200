47d81875caa27b46796b19c41b38e7bf7c19806e
K2381 <K2381@student.jamk.fi>
2017-10-04 10:52:15 +0300

    Demoja

---
+++ TheThing/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheThing
+{
+    class Thing
+    {
+        //Julkinen jäsenmuuttuja != ominaisuus
+        // public string Name;
+        //Parempi vaihtoehto, ominaisuus AutoImplemented propertyn avulla
+        //public string Name { get; set; }
+        //ToString() - metodi käyttää jäsenmuuttujaa tai propertyä
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //Constuctors
+
+        public Thing()
+        {
+            name = "nobody here";
+        }
+
+
+
+        public override string ToString()
+        {
+           // return "Nimeni on: " + Name;
+            //mutta mieluummin jos on sisäinen muuuttuja niin käytetään sitä
+            return "Nimeni on:" + name;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se - jostakin testaus
+            //Thing t = new Thing();
+            //Console.WriteLine(t.ToString());
+            //t.Name = "Jotakin";
+            //Console.WriteLine(t.Name);
+            //Console.WriteLine(t.ToString());
+            //Student - olion testaus
+
+            Student s = new Student();
+            s.FirstName = "Esa";
+            s.LastName = "Salmikangas";
+            Student s2 = new Student("Jaska", "Jokunen", "K2323");
+            //Luodaan kokoelma student-olio käytetään list
+            List<Student> students = new List<Student>();
+            students.Add(s);
+            students.Add(s2);
+            students.Add(new Student("Arska", "Aaltonen", "k2323"));
+            // Näytetään oppilaat
+            Console.WriteLine("Luokan oppilaat: ");
+            foreach (Student item in students)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Group luokka = new Group();
+            luokka.Name = "TTV16S1";
+            luokka.Students.Add(new Student("Anna", "Aurinko", "K233"));
+            Console.WriteLine(luokka.ToString());
+
+        }
+    }
+}
---
+++ TheThing/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheThing
+{
+   public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AsioID { get; set; }
+
+        public Student()
+        {}
+
+        public Student(string fname,string lname,string asio)
+        {
+            FirstName = fname;
+            LastName = lname;
+            AsioID = asio;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " Asio: " + AsioID;
+        }
+
+        
+    }
+
+    public class Group
+    {
+        public string Name { get; set; }
+        public List<Student> Students;
+        //Konstruktori
+
+        public Group()
+        {
+            Students = new List<Student>();
+            //Luettaisiin esimerkiksi tietokannasta ne oppilaitten tiedot
+            Students.Add(new Student("Pelle", "Peloton", "AnkkaLinna"));
+            Students.Add(new Student("Aku", "Ankka", "AnkkaLinna"));
+            Students.Add(new Student("Mikki", "Hiiri", "AnkkaLinna"));
+
+            //ylikirjoitetaan ToString
+        }
+            public override string ToString()
+        {
+            string retval = "Luokka " + Name + "Sisältää oppilaat\n ";
+            foreach (Student item in Students)
+            {
+                retval += item.ToString() + "\n";
+            }
+            return retval;
+        }
+    }
+    }
