1170c68a7f24447f2a367d1597fa35bc1c621a8a
K2381 <K2381@student.jamk.fi>
2017-11-01 10:56:17 +0200

    demoja

---
+++ LaskinTests/MyCalculatorTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JAMK.IT.Laskin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.Laskin.Tests
+{
+    [TestClass()]
+    public class MyCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            // Testataan MyCalculator luokan Add-metodia, AAA-malli
+            // Arrange
+            int i = 10;
+            int j = 12;
+            int tulos = 22;
+            MyCalculator laskin = new MyCalculator();
+            // Act
+            int summa = laskin.Add(i, j);
+            //Assert
+            Assert.AreEqual(tulos, summa);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            // Testataan MyCalculator luokan Multiply-metodia, AAA-malli
+            // Arrange
+            int i = 10;
+            int j = 12;
+            int tulos = 120;
+            MyCalculator laskin = new MyCalculator();
+            // Act
+            int summa = laskin.Multiply(i, j);
+            //Assert
+            Assert.AreEqual(tulos, summa);
+            //  Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            int i = 24;
+            int j = 4;
+            int odotettu = 6;
+            MyCalculator laskin = new MyCalculator();
+            int jako = laskin.Divide(i, j);
+
+            Assert.AreEqual(odotettu,jako);
+        }
+
+        [TestMethod()]
+        public void DivideTestWithZero()
+        {
+            int i = 24;
+            int j = 0;
+            int odotettu = 1;
+            MyCalculator laskin = new MyCalculator();
+            try
+            {
+                int jako = laskin.Divide(i, j);
+                Assert.Fail();
+            }
+            catch(DivideByZeroException)
+            {
+                //ok , tulee oikean tyyppinen poikkeus
+            }
+            catch (Exception )
+            {
+                Assert.Fail();
+
+            }
+
+            DivideByZeroException dbze = new DivideByZeroException();
+            
+        }
+    }
+}
---
+++ LaskinTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LaskinTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaskinTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae55298e-3247-451d-8b34-0734f5a36ced")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
